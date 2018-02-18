5fad59d25d2401c055fbe51e1cd37231f4aac418
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-02-18 15:23:39 -0500

    Create task_14.py

---
+++ task_14.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""equality operator and logical AND operator"""
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+print IS_TRUE
+
+print IS_FALSE
+
+print IS_NONE
+
+INTEGER_EQUIV = (IS_TRUE == 1 and IS_FALSE == 0)
+
+print INTEGER_EQUIV
