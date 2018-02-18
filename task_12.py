ad15aed4879f9e693ef559dc06fe5d183e21f5e9
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-02-18 15:22:15 -0500

    Create task_12.py

---
+++ task_12.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Assigning numeric types and importing statements"""
+
+INTVAL = 1
+
+FLOATVAL = 0.1
+
+import decimal
+
+DECVAL = decimal.Decimal('0.1')
+
+import fractions
+
+FRACVAL = fractions.Fraction(1,10)
+
+print INTVAL
+
+print FLOATVAL
+
+print DECVAL
+
+print FRACVAL
